b367b068eb36b94bff33bb66b961d451114d7cb7
bachquanghung <115093833+bachquanghung@users.noreply.github.com>
2023-05-20 23:29:27 +0700

    Update TodoList.js

--- Question1/src/TodoList.js
+++ Question1/src/TodoList.js
@@ -2,31 +2,20 @@ import { useState, useEffect } from "react";
 
 
 const TodoList = () => {
-    const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
   const [listTasks, setListTasks] = useState([]);
-  const [completed, setCompleted] = useState(0);
+  const [completedTasks, setCompletedTasks] = useState(0); 
+  const [language, setLanguage ] =useState(false)
 
   useEffect(() => {
-    const localTask = localStorage.getItem("listTasks");
-    if (localTask) {
-      setListTasks(JSON.parse(localTask));
+    const taskLocal = localStorage.getItem("listTasks");
+    if (taskLocal) {
+      setListTasks(JSON.parse(taskLocal));
     }
   }, []);
-
-  
-
-  const handleCheck = (index) => {
-    const newListTasks = [...listTasks];
-    newListTasks[index].completed = !newListTasks[index].completed;
-    setListTasks(newListTasks);
-
-    const newCompletedTasks = newListTasks.filter((task) => task.completed)
-      .length;
-    setCompleted(newCompletedTasks);
-
-    localStorage.setItem("listTasks", JSON.stringify(newListTasks));
-  };
-
+const Change=()=>{
+  setLanguage(language===false?true:false)
+}
   const handleSubmit = () => {
     const newListTasks = [...listTasks, { task: input, completed: false }];
     setListTasks(newListTasks);
@@ -35,28 +24,89 @@ const TodoList = () => {
     localStorage.setItem("listTasks", JSON.stringify(newListTasks));
   };
 
+  const handleCheckboxClick = (index) => {
+    const newListTasks = [...listTasks];
+    newListTasks[index].completed = !newListTasks[index].completed;
+    setListTasks(newListTasks);
+
+    const newCompletedTasks = newListTasks.filter(
+      (task) => task.completed
+    ).length;
+    setCompletedTasks(newCompletedTasks);
+
+    localStorage.setItem("listTasks", JSON.stringify(newListTasks));
+  };
+
   return (
-    <div className>
+   <>
+   {!language?( <div className="wrapper">
+      <button className="language" onClick={Change}>Change language</button>
       <div className="box">
-       
+        <div className="tasks">
+          You have {listTasks.length - completedTasks} tasks left!
+        </div>
         <div className="list-task">
-          {listTasks.map((task, index) => (
-            <div key={index} className={task.completed?'todo-item-container done':"todo-item-container"}>
-              <input type="checkbox" onClick={() => handleCheck(index)} checked={task.completed} />
-              <span style={{marginLeft:'10px'}}>{task.task}</span>
+          {listTasks.map((listTask, index) => (
+            <div
+              key={index}
+              className={`task ${listTask.completed && "completed"}`}
+            >
+              <input
+                type="checkbox"
+                onClick={() => handleCheckboxClick(index)}
+                checked={listTask.completed}
+              />
+              <span>{listTask.task}</span>
             </div>
           ))}
         </div>
-        
-        <form className="form">
-        <input value={input}
+        <div className="input">
+          <input
+            value={input}
             onChange={(e) => setInput(e.target.value)}
             type="text"
-            placeholder="Enter task ..."/>
-        <button onClick={handleSubmit}>Submit</button>
-      </form>
+            placeholder="Enter task ..."
+          />
+          <button onClick={handleSubmit} className="submit">
+            Submit
+          </button>
+        </div>
       </div>
-    </div>
+    </div>):( <div className="wrapper">
+      <button className="language" onClick={Change}>Đổi ngôn ngữ</button>
+      <div className="box">
+        <div className="tasks">
+         Bạn có {listTasks.length - completedTasks} nhiệm vụ!
+        </div>
+        <div className="list-task">
+          {listTasks.map((listTask, index) => (
+            <div
+              key={index}
+              className={`task ${listTask.completed && "completed"}`}
+            >
+              <input
+                type="checkbox"
+                onClick={() => handleCheckboxClick(index)}
+                checked={listTask.completed}
+              />
+              <span>{listTask.task}</span>
+            </div>
+          ))}
+        </div>
+        <div className="input">
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="Thiết lập nhiệm vụ ..."
+          />
+          <button onClick={handleSubmit} className="submit">
+            Thiết lập
+          </button>
+        </div>
+      </div>
+    </div>)}
+   </>
   );
 };
 
